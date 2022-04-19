063bef90bf9db5a6e55fb92512359308f3a5f2cd
박예찬 <jkyyc3@gmail.com>
2022-04-19 23:25:51 +0900

    UPDATE : GET Query

--- fast-campus/spring-boot-basic/hello/src/main/java/com/example/hello/controller/GetApiController.java
+++ fast-campus/spring-boot-basic/hello/src/main/java/com/example/hello/controller/GetApiController.java
@@ -1,6 +1,9 @@
 package com.example.hello.controller;
 
+import com.example.hello.dto.UserRequest;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/get-practice") // RequestMapping 은 모든 Method 에 응답한다
@@ -26,5 +29,46 @@ public class GetApiController {
     }
 
     // Query Parameter
+    @GetMapping(path="query-param")
+    public String queryParam(@RequestParam Map<String, String> queryParam) {
+
+        StringBuilder sb = new StringBuilder();
+
+        queryParam.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            System.out.println("\n");
+
+            sb.append(entry.getKey() + " = " + entry.getValue()+"\n");
+        });
+
+        return sb.toString();
+    }
+
+    @GetMapping(path="query-param-exact")
+    public String queryParamExact(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam int age
+    ) {
+
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(age);
+
+        return name + " "  + email + " " + age;
+    }
+
+    // 현업에서 가장 많이 사용하는 방식은 DTO
+    // Request Param은 붙지 않는다.
+    // 알아서 ? 뒤를 파악하고 변수의 이름과 매칭한다
+    @GetMapping(path="query-dto")
+    public String queryDto(UserRequest userRequest) {
+        System.out.println(userRequest.getName());
+        System.out.println(userRequest.getEmail());
+        System.out.println(userRequest.getAge());
+
+        return userRequest.toString();
+    }
 
 }
