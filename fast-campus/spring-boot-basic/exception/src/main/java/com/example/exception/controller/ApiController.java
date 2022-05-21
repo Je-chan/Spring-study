ad65eda3ee76e04e67c5ed47d8aa2bc9fc5ebf46
박예찬 <jkyyc3@gmail.com>
2022-05-21 19:41:24 +0900

    CREATE : Init Spring Core Practice

--- fast-campus/spring-boot-basic/exception/src/main/java/com/example/exception/controller/ApiController.java
+++ fast-campus/spring-boot-basic/exception/src/main/java/com/example/exception/controller/ApiController.java
@@ -1,17 +1,28 @@
 package com.example.exception.controller;
 
 import com.example.exception.dto.User;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @RestController
 @RequestMapping("/api/user")
+
+// 변수들을 검증할 때 Validated 를 사용하면 된다 (DTO 가 아닌 경우)
+@Validated
 public class ApiController {
 
     @GetMapping("")
     // required = false 를 사용하면 꼭 필수가 아니게 된다.
-    public User get(@RequestParam String name, @RequestParam(required = false) Integer age) {
+    public User get(
+            @Size(min=1)
+            @RequestParam String name,
+
+            @NotNull
+            @RequestParam(required = false) Integer age) {
         User user = new User();
         user.setName(name);
         user.setAge(age);
