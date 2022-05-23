70b90e7513ebe1dfb2ad59847e018c13328e863c
박예찬 <jkyyc3@gmail.com>
2022-05-23 23:15:51 +0900

    CREATE : Rate Discount

---
+++ inflearn/spring-core-practice/src/main/java/com/springstudy/springcorepractice/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package com.springstudy.springcorepractice.discount;
+
+import com.springstudy.springcorepractice.member.Grade;
+import com.springstudy.springcorepractice.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
