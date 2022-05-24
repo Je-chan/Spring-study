99213973d0ccc4f93edeb278d871994a4dd3ac6d
박예찬 <jkyyc3@gmail.com>
2022-05-24 21:22:42 +0900

    REFACTOR : Constructor

--- inflearn/spring-core-practice/src/main/java/com/springstudy/springcorepractice/order/AppConfig.java
+++ inflearn/spring-core-practice/src/main/java/com/springstudy/springcorepractice/order/AppConfig.java
@@ -1,5 +1,6 @@
 package com.springstudy.springcorepractice.order;
 
+import com.springstudy.springcorepractice.discount.DiscountPolicy;
 import com.springstudy.springcorepractice.discount.FixDiscountPolicy;
 import com.springstudy.springcorepractice.member.MemberService;
 import com.springstudy.springcorepractice.member.MemberServiceImpl;
@@ -13,7 +14,7 @@ import com.springstudy.springcorepractice.member.MemoryMemberRepository;
     // 클라이언트 코드에는 따로 로직 수정을 할 필요가 없어진다
 // AppConfig 는 공연 기획자. 구체 클래스를 선택한다(배역에 맞는 배우를 선택한다)
 // 애플리케이션이 어떻게 동작해야 할지 전체 구성을 책임진다
-ㄴ
+
 public class AppConfig {
     // 객체의 생성과 연결을 담당하게 된다
         // MemberServiceImpl 로 객체를 생성하고
@@ -21,11 +22,19 @@ public class AppConfig {
     // 이렇게 해주는 게 관심사를 분리하는 것. 즉, 객체를 생성하고 연결하는 역할과 실행하는 역할이 분리된 것
     // 이를 한 마디로 표현하면 Dependency Injection = DI, 의존관계 주입이라고 한다
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 
